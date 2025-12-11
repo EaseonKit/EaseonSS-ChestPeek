2982fc6f037e2f97d3514e7763d362a03013f1bc
Teron <214763666+EaseonKit@users.noreply.github.com>
2025-12-11 18:02:34 +0900

    1.21.11 - Initial commit

--- src/main/java/com/easeon/ss/chestpeek/ChestPeekHandler.java
+++ src/main/java/com/easeon/ss/chestpeek/ChestPeekHandler.java
@@ -4,64 +4,66 @@ import com.easeon.ss.core.wrapper.EaseonBlockHit;
 import com.easeon.ss.core.wrapper.EaseonEntity;
 import com.easeon.ss.core.wrapper.EaseonPlayer;
 import com.easeon.ss.core.wrapper.EaseonWorld;
-import net.minecraft.block.*;
-import net.minecraft.block.entity.LootableContainerBlockEntity;
-import net.minecraft.entity.decoration.ItemFrameEntity;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.decoration.ItemFrame;
+import net.minecraft.world.level.block.ChestBlock;
+import net.minecraft.world.level.block.EnderChestBlock;
+import net.minecraft.world.level.block.WallSignBlock;
+import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 
 public class ChestPeekHandler {
-    public static ActionResult useEntityCallback(EaseonWorld world, EaseonPlayer player, Hand hand, EaseonEntity entity) {
-        if (world.isClient()) return ActionResult.SUCCESS;
-        if (hand != Hand.MAIN_HAND || player.isSneaking() || !(entity.get() instanceof ItemFrameEntity itemFrame))
-            return ActionResult.PASS;
+    public static InteractionResult useEntityCallback(EaseonWorld world, EaseonPlayer player, InteractionHand hand, EaseonEntity entity) {
+        if (world.isClient()) return InteractionResult.SUCCESS;
+        if (hand != InteractionHand.MAIN_HAND || player.isSneaking() || !(entity.get() instanceof ItemFrame itemFrame))
+            return InteractionResult.PASS;
 
-        if (itemFrame.getHeldItemStack().isEmpty() && !player.getStackInHand(hand).isEmpty())
-            return ActionResult.PASS;
+        if (itemFrame.getItem().isEmpty() && !player.getStackInHand(hand).isEmpty())
+            return InteractionResult.PASS;
 
-        final var facing = itemFrame.getHorizontalFacing().getOpposite();
-        final var behind = itemFrame.getBlockPos().offset(itemFrame.getHorizontalFacing().getOpposite());
+        final var facing = itemFrame.getDirection().getOpposite();
+        final var behind = itemFrame.getPos().relative(itemFrame.getDirection().getOpposite());
 
         return OpenChest(world, player, hand, facing, behind);
     }
 
-    public static ActionResult useBlockCallback(EaseonWorld world, EaseonPlayer player, Hand hand, EaseonBlockHit hit) {
-        if (world.isClient()) return ActionResult.SUCCESS;
-        if (player.isSneaking()) return ActionResult.PASS;
+    public static InteractionResult useBlockCallback(EaseonWorld world, EaseonPlayer player, InteractionHand hand, EaseonBlockHit hit) {
+        if (world.isClient()) return InteractionResult.SUCCESS;
+        if (player.isSneaking()) return InteractionResult.PASS;
 
         final var pos = hit.getBlockPos();
         final var state = world.getBlockState(pos);
         if (state.not(WallSignBlock.class))
-            return ActionResult.PASS;
+            return InteractionResult.PASS;
 
         final var facing = state.get(WallSignBlock.FACING);
-        final var behind = pos.offset(facing.getOpposite());
+        final var behind = pos.relative(facing.getOpposite());
 
         return OpenChest(world, player, hand, facing, behind);
     }
 
-    private static ActionResult OpenChest(EaseonWorld world, EaseonPlayer player, Hand hand, Direction direction, BlockPos behind)
+    private static InteractionResult OpenChest(EaseonWorld world, EaseonPlayer player, InteractionHand hand, Direction direction, BlockPos behind)
     {
         final var block = world.getBlockState(behind);
         if (block.of(ChestBlock.class, EnderChestBlock.class)) {
-            var actionResult = block.onUse(world, player, new BlockHitResult(Vec3d.ofCenter(behind), direction, behind, false));
-            if (actionResult.isAccepted()) {
+            var interactionResult = block.onUse(world, player, new BlockHitResult(Vec3.atCenterOf(behind), direction, behind, false));
+            if (interactionResult.consumesAction()) {
                 player.swingHand(hand);
-                return ActionResult.SUCCESS;
+                return InteractionResult.SUCCESS;
             }
         }
 
         final var blockEntity = world.getBlockEntity(behind);
-        if (blockEntity instanceof LootableContainerBlockEntity chest) {
+        if (blockEntity instanceof RandomizableContainerBlockEntity chest) {
             player.openHandledScreen(chest);
             player.swingHand(hand);
-            return ActionResult.SUCCESS;
+            return InteractionResult.SUCCESS;
         }
 
-        return ActionResult.PASS;
+        return InteractionResult.PASS;
     }
 }
